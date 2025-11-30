471a9b9941ed213c179cb52cb91d8207a4155b3e
Wendy <wendy.mermet@gmail.com>
2025-11-30 15:08:04 +0100

    Docstrings added to all functionsand classes. Last check performed. Code is ready for hand in.

--- src/HealthAnalyzeTools.py
+++ src/HealthAnalyzeTools.py
@@ -10,11 +10,22 @@ from sklearn.linear_model import LinearRegression
 
 class HealthAnalyzer:
     def __init__(self):
+        """Initializes the HealthAnalyzer class and loads the health study dataset.
+        This method loads the dataset from a CSV file and stores it in the instance variable `df`.
+        Returns:
+            None
+        """
         df = io.load_data("Data\health_study_dataset.csv")
         self.df = df
     
 
     def main_summary(self):
+        """Prints summary statistics for the health study dataset.
+        This method calculates and prints the mean, standard deviation, minimum, maximum, and count for the columns:
+        'age', 'height', 'weight', 'systolic_bp', and 'cholesterol'.
+        Returns:
+            None
+        """
         age = io.df["age"].to_numpy()
         print("In column \"age\":")
         mt.statistics(age)
@@ -36,25 +47,59 @@ class HealthAnalyzer:
         mt.statistics(cholesterol)
         
     def plot_blood_pressure_histogram(self) -> None:
+        """Plots a histogram of the systolic blood pressure from the health study dataset.
+        This method uses the `plot_blood_pressure_histogram` function from the `visuals` module to create a histogram.
+        Returns:
+            None: Displays the histogram plot.
+        """
         return vis.plot_blood_pressure_histogram(df=self.df)
 
     def plot_weight_boxplot(self) -> None:
+        """Plots a boxplot of the weight from the health study dataset.
+        This method uses the `plot_weight_boxplot` function from the `visuals` module to create a boxplot.
+        Returns:
+            None: Displays the boxplot.
+        """
         return vis.plot_weight_boxplot(df=self.df)
 
     def plot_smoker_bar_chart(self) -> None:
+        """Plots a bar chart showing the number of smokers and non-smokers in the health study dataset.
+        This method uses the `plot_smoker_bar_chart` function from the `visuals` module to create the bar chart.
+        Returns:
+            None: Displays the bar chart.
+        """
         return vis.plot_smoker_bar_chart(df=self.df)
 
 class HealthStatistics:
     def __init__(self):
+        """Initializes the HealthStatistics class and loads the health study dataset.
+        This method loads the dataset from a CSV file and stores it in the instance variable `df`.
+        Returns:
+            None
+        """
         df = io.load_data("Data\health_study_dataset.csv")
         self.df = df
 
     def perc_sick_participants(self) -> float:
+        """Calculates the percentage of participants with a disease in the health study dataset.
+        This method filters the dataset to find participants with a disease (where 'disease' is
+        not equal to 0) and calculates the percentage of such participants relative to the total number of participants.
+        Returns:
+            float: Percentage of participants with a disease.
+        """
         sick_df = df[df['disease'] != 0]
         return len(sick_df)/len(df)
     
     
     def simulate_disease(self, n: int = 1000) -> tuple[float, float]:
+        """Simulates the occurrence of a disease in a population of size n.
+        This method generates a random sample of disease occurrences based on the mean disease rate in the dataset.
+        Args:
+            n (int, optional): Size of the population to simulate. Defaults to 1000
+        Returns:
+            tuple: A tuple containing the mean of the simulated diseases and the mean of the actual disease
+            occurrences in the dataset.
+        """
         np.random.seed(42)
         mean_sick_df = self.df["disease"].mean()
         diseases = np.random.choice([0, 1], size=n, p=[1 - mean_sick_df, mean_sick_df])
@@ -65,6 +110,13 @@ class HealthStatistics:
     x = np.random.choice(df["systolic_bp"], size = 5000, replace = True)
 
     def ci_mean_normal (self, x, confidence= 0.95):
+        """Calculates the confidence interval for the mean of a sample using the normal approximation.
+        Args:
+            x (array-like): Sample data for which to calculate the confidence interval.
+            confidence (float, optional): Confidence level for the interval. Defaults to 0.95.
+        Returns:
+            tuple: A tuple containing the lower bound, upper bound, mean, standard deviation, and sample size.
+        """
         x = np.asarray(x, dtype = float)
         mean_x = float(np.mean(x))
         s = float(np.std(x,ddof = 1))
@@ -75,9 +127,25 @@ class HealthStatistics:
         return lo, hi, mean_x, s, n
     
     def ci_mean_normal_graph(self, lo, hi, mean_x) -> None:
+        """Creates a graph to visualize the confidence interval for the mean.
+        Args:
+            lo (float): Lower bound of the confidence interval.
+            hi (float): Upper bound of the confidence interval.
+            mean_x (float): Mean of the sample data.
+        Returns:
+            None: Displays the graph showing the confidence interval.
+        """
         return vis.ci_mean_normal_graph(lo, hi, mean_x)
 
     def ci_mean_bootstrap(self, x,B= 5_000,confidence=0.95):
+        """Calculates the confidence interval for the mean of a sample using bootstrap resampling.
+        Args:
+            x (array-like): Sample data for which to calculate the confidence interval.
+            B (int, optional): Number of bootstrap samples to generate. Defaults to 5,000.
+            confidence (float, optional): Confidence level for the interval. Defaults to 0.95.
+        Returns:
+            tuple: A tuple containing the lower bound, upper bound, and mean of the bootstrap samples.
+        """
         x = np.asarray(x, dtype = float)
         n= len(x)
         boot_means = np.empty(B)
@@ -90,10 +158,24 @@ class HealthStatistics:
         return float(lo), float(hi), float(mean_x)
     
     def ci_mean_boot_graph(self, lo, hi, mean_x) -> None:
+        """Creates a graph to visualize the confidence interval for the mean using bootstrap resampling.
+        Args:
+            lo (float): Lower bound of the confidence interval.
+            hi (float): Upper bound of the confidence interval.
+            mean_x (float): Mean of the sample data.
+        Returns:
+            None: Displays the graph showing the confidence interval.
+        """
         return vis.ci_mean_boot_graph(lo, hi, mean_x)
 
      
     def hypothesis_test_smoker_bp(self):
+        """Performs a hypothesis test to compare the mean systolic blood pressure between smokers and non-smokers.
+        This method uses bootstrap resampling to estimate the p-value and confidence interval for the difference in means.
+        Returns:
+            tuple: A tuple containing the p-value, lower bound, upper bound of the confidence interval, and the true difference in means.
+        """
+
         A = df.loc[df.smoker == "Yes", "systolic_bp"].values
         B = df.loc[df.smoker == "No", "systolic_bp"].values
         mu_A = np.mean(A)
@@ -113,6 +195,11 @@ class HealthStatistics:
     
 
     def cohen_d_smoker_bp(self):
+        """Calculates Cohen's d for the difference in mean systolic blood pressure between smokers and non-smokers.
+        This method computes the means and pooled standard deviation for the two groups and returns the effect size.
+        Returns:
+            tuple: A tuple containing Cohen's d and the number of participants in each group.
+        """
         A = df.loc[df.smoker == "Yes", "systolic_bp"].values
         B = df.loc[df.smoker == "No", "systolic_bp"].values
         mu_A = np.mean(A)
@@ -123,12 +210,39 @@ class HealthStatistics:
         return d, n_per_group
     
     def plot_regression_weight_bp(self, predictions) -> None:
+        """Plots a regression line of weight against systolic blood pressure.
+        Args:
+            predictions (np array of float): Predicted values from a linear regression model.
+        Returns:
+            fig (plt.Figure): Matplotlib Figure object containing the scatter plot and regression line.
+        """
         return vis.plot_regression_weight_bp(df=self.df, predictions=predictions)
     
     def plot_regression_age_bp(self, predictions) -> None:
+        """Plots a regression line of age against systolic blood pressure.
+        Args:
+            predictions (np array of float): Predicted values from a linear regression model.
+        Returns:
+            fig (plt.Figure): Matplotlib Figure object containing the scatter plot and regression line.
+        """
         return vis.plot_regression_age_bp(df=self.df, predictions=predictions)
     
+    def plot_predictions_bp(self, predictions) -> None:
+        """Plots a scatter plot of predicted blood pressure against actual blood pressure.
+        Args:
+            predictions (np array of float): Predicted values from a linear regression model.
+        Returns:
+            fig (plt.Figure): Matplotlib Figure object containing the scatter plot.
+        """
+        return vis.plot_predictions_bp(df=self.df, predictions=predictions)
+    
     def linear_regression_age_weight_bp(self):
+        """Performs linear regression to predict systolic blood pressure based on age and weight.
+        This method fits a linear regression model using age and weight as predictors and returns the model coefficients,
+        intercept, R-squared value, and predictions.
+        Returns:
+            tuple: A tuple containing the intercept, coefficient for age, coefficient for weight, R-squared value, and predictions.
+        """
         x_2d = df[['age', 'weight']].values
         y = df['systolic_bp'].values
 
@@ -142,6 +256,13 @@ class HealthStatistics:
         return intercept, coef_age, coef_weight, r_squared, predictions
         
     def corr_age_weight_bp(self):
+        """Calculates the correlation coefficients between age, weight, and systolic blood pressure.
+        This method computes the Pearson correlation coefficients between age and systolic blood pressure,
+        and between weight and systolic blood pressure.
+        Returns:
+            tuple: A tuple containing the correlation coefficient between age and systolic blood pressure,
+            and the correlation coefficient between weight and systolic blood pressure.
+        """
         corr_age_bp = np.corrcoef(df['age'], df['systolic_bp'])[0, 1]
         corr_weight_bp = np.corrcoef(df['weight'], df['systolic_bp'])[0, 1]
         return corr_age_bp, corr_weight_bp
--- src/visuals.py
+++ src/visuals.py
@@ -5,7 +5,13 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 
 def plot_blood_pressure_histogram(df: pd.DataFrame) -> None:
-    """Plots a histogram of systolic blood pressure."""
+    """Plots a histogram of blood pressure readings.
+
+    Args:
+        df (pd.DataFrame): health data DataFrame.
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the histogram.
+    """
     def _draw(ax):
         ax.hist(df["systolic_bp"],color='pink' ,bins=40)
         ax.set_xlabel("Blood pressure in mmHg")
@@ -24,7 +30,14 @@ def plot_blood_pressure_histogram(df: pd.DataFrame) -> None:
 
 
 def plot_weight_boxplot(df: pd.DataFrame) -> None:
-    """Plots a boxplot of weight by gender."""
+    """Boxplot of weight by gender
+
+    Args:
+        df (pd.DataFrame):  health data DataFrame
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the boxplot.
+    """
+
     def _draw(ax):
         df.boxplot(column="weight", by="sex", ax=ax, grid=False)
         ax.set_xlabel("Gender")
@@ -50,9 +63,15 @@ smoker_df = df[df['smoker'] != 'No']
 non_smoker_df = df[df['smoker'] == 'No']
 len(smoker_df)
 len(non_smoker_df)
-
+#Take df from function arguments
 def plot_smoker_bar_chart(df: pd.DataFrame) -> None:
-    """Plots a bar chart showing number of smokers and non-smokers."""
+    """Bar chart Smokers, non smokers
+
+    Args:
+        df (pd.DataFrame): health data DataFrame
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the bar chart.
+    """
     def _draw(ax):
         ax.bar(['Yes', 'No'], [len(smoker_df), len(non_smoker_df)],color=['lightblue', 'lightgreen'])
         ax.set_xlabel("Smoker")
@@ -69,6 +88,15 @@ def plot_smoker_bar_chart(df: pd.DataFrame) -> None:
    
 
 def ci_mean_normal_graph(lo, hi, mean_x) -> None:
+    """Plots a graph showing the confidence interval for the mean blood pressure using normal approximation.
+
+    Args:
+        lo (float): lowest point of the confidence interval
+        hi (float): highest point of the confidence interval
+        mean_x (float): mean blood pressure
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the confidence interval graph.
+    """
     def _draw(ax):
         ax.errorbar([0], [mean_x], yerr=[[mean_x - lo], [hi - mean_x]], fmt="o", capsize=6)
         ax.set_title("95%- CI for the average (normal -approximation)\n for blood pressure")
@@ -85,6 +113,14 @@ def ci_mean_normal_graph(lo, hi, mean_x) -> None:
     return fig
 
 def ci_mean_boot_graph(lo, hi, mean_x) -> None:
+    """Plots a graph showing the confidence interval for the mean blood pressure using bootstrap method.
+    Args:
+        lo (float): lowest point of the confidence interval
+        hi (float): highest point of the confidence interval
+        mean_x (float): mean blood pressure
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the confidence interval graph.
+    """
     def _draw(ax):
         ax.errorbar([0], [mean_x], yerr=[[mean_x - lo], [hi - mean_x]], fmt="o", capsize=6)
         ax.set_title("95%- CI for the average (bootstrap)\n for blood pressure")
@@ -102,6 +138,14 @@ def ci_mean_boot_graph(lo, hi, mean_x) -> None:
 
 
 def plot_regression_weight_bp(df: pd.DataFrame, predictions) -> None:
+    """Plots a scatter plot of blood pressure against weight with a regression line.
+
+    Args:
+        df (pd.DataFrame): health data DataFrame containing 'weight' and 'systolic_bp'.
+        predictions (np array of float): predicted values from a linear regression model.
+    Returns:    
+        fig (plt.Figure): Matplotlib Figure object containing the scatter plot and regression line.
+    """
     def _draw(ax):
         ax.scatter(df['weight'], df['systolic_bp'], alpha=0.2, label='Blood Pressure per weight', color='purple')
         ax.scatter(df['weight'], predictions, color='hotpink', label='Regression Line')
@@ -118,6 +162,14 @@ def plot_regression_weight_bp(df: pd.DataFrame, predictions) -> None:
     return fig
 
 def plot_regression_age_bp(df: pd.DataFrame, predictions) -> None:
+    """Plots a scatter plot of blood pressure against age with a regression line.
+
+    Args:
+        df (pd.DataFrame): health data DataFrame containing 'age' and 'systolic_bp'.
+        predictions (np array of float): predicted values from a linear regression model.
+    Returns: 
+        fig (plt.Figure): Matplotlib Figure object containing the scatter plot and regression line.
+    """
     def _draw(ax):
         ax.scatter(df['age'], df['systolic_bp'], alpha=0.6, label='Blood Pressure vs Age', color='pink')
         ax.scatter(df['age'], predictions, color='hotpink', alpha=1, label='Regression Line')
@@ -133,7 +185,39 @@ def plot_regression_age_bp(df: pd.DataFrame, predictions) -> None:
     plt.close(fig)
     return fig
 
+def plot_predictions_bp(df: pd.DataFrame, predictions) -> None:
+    """Plots a scatter plot of predicted blood pressure against actual blood pressure.
+    Args:
+        df (pd.DataFrame): health data DataFrame containing 'systolic_bp'.
+        predictions (np array of float): predicted values from a linear regression model.
+    Returns:
+        fig (plt.Figure): Matplotlib Figure object containing the scatter plot.
+    """
+    def _draw(ax):
+        ax.scatter(predictions, df['systolic_bp'], alpha=0.6, label='Predicted Blood Pressure vs Blood Pressure', color='pink')
+        # ax.scatter(df['age'], predictions, color='hotpink', alpha=1, label='Regression Line')
+        ax.set_xlabel('Predicted Systolic Blood Pressure (mmHg)')
+        ax.set_ylabel('Systolic Blood Pressure (mmHg)')
+        ax.set_title('Linear Regression: Predicted Blood Pressure vs Blood Pressure')
+        ax.legend()
+
+    fig, ax = plt.subplots()
+    _draw(ax)
+    plt.tight_layout()
+    fig.replot = _draw
+    plt.close(fig)
+    return fig
+
 def add_to_dashboard(figures, cols=2, figsize=(12, 8),title="Dashboard"):
+    """Creates a dashboard figure with multiple subplots from a list of figures.
+    Args:
+        figures (list): List of Matplotlib Figure objects or Axes objects to be added to the dashboard.
+        cols (int): Number of columns in the dashboard layout.
+        figsize (tuple): Size of the dashboard figure.
+        title (str): Title for the dashboard figure.
+    Returns:
+        None: Displays the dashboard figure with all subplots.
+    """
     rows = (len(figures) + cols - 1) // cols
     dashboard_fig, axes = plt.subplots(rows, cols, figsize=figsize, sharex=False, sharey=False)
     
